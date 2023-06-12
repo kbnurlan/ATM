895a205dcd5a195fe2bf7f0a672116b1096378ef
kbnurlan <135407853+kbnurlan@users.noreply.github.com>
2023-06-12 12:24:57 +0400

    another_rscript.R

--- another_rscript.R
+++ another_rscript.R
@@ -4,7 +4,7 @@
 system('git add -A')
 system('git commit -m "all"')
 
-system(paste('git push https://',Sys.getenv('GIT_ID'),'@github.com/kbnurlan/ATM_A.git',sep = ''))
+system(paste('git push https://',Sys.getenv('GIT_ID'),'@github.com/kbnurlan/ATM.git',sep = ''))
 
 
 
