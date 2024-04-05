c14d597e257505f09bacb903bfd448b629ecfd11
ATM optimization <ATM_optimization@kapitalho.az>
2024-04-05 17:48:51 +0400

    all

--- path_change.R
+++ path_change.R
@@ -1,6 +1,8 @@
 library(fs)
 id <- "\\\\Filesrv07\\head-office\\Private\\Tehlil_ve_koordinasiya_shob\\Tehlil_ve_koordinasiya_shobesi\\texniki_id\\chat_id.xlsx"
-path <- '\\\\10.0.6.184\\nt\\Daily_FIMI\\FIMI_new\\ATM\\chat_id.xlsx'
-
+path1 <- '\\\\10.0.6.184\\nt\\Daily_FIMI\\FIMI_new\\ATM\\chat_id.xlsx'
+path2 <- '\\\\10.0.6.184\\nt\\Daily_FIMI\\fimi\\chat_id.xlsx'
 file_copy(id, path, overwrite = T)
+file_copy(id, path2, overwrite = T)
+
 
